a882826249c1d2844aa027bbc6f2dbde97b0bc99
Tetiana Yarova <andronikova11@gmail.com>
2021-09-20 21:53:39 +0300

    Install npm, package nodemon;
    added scripts to package.json
    Creates a folder "db" with file contacts.json

---
+++ contacts.js
@@ -0,0 +1,7 @@
+const fs = require("fs");
+const path = require("path");
+
+// const contactsPath = path.join(_dirname, "db", "contacts.json");
+
+// console.log(contactsPath);
+console.log("Name of directory", path.dirname(__filename));
