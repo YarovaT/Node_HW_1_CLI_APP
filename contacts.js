f254a1fe08a1c982d2aad93a85b3a2cf7d40976b
Tetiana Yarova <andronikova11@gmail.com>
2021-09-21 22:15:01 +0300

    Create contactPath, use method path

--- contacts.js
+++ contacts.js
@@ -1,7 +1,28 @@
 const fs = require("fs");
 const path = require("path");
 
-// const contactsPath = path.join(_dirname, "db", "contacts.json");
+const contactsPath = path.join(__dirname, "db", "contacts.json");
 
-// console.log(contactsPath);
-console.log("Name of directory", path.dirname(__filename));
+// TODO: задокументировать каждую функцию
+function listContacts() {
+  fs.readFile(contactsPath, "utf-8", (err, content) => {
+    if (err) {
+      throw err;
+    }
+    console.log(content);
+  });
+}
+
+console.log(listContacts());
+
+function getContactById(contactId) {
+  // ...твой код
+}
+
+//   function removeContact(contactId) {
+//     // ...твой код
+//   }
+
+//   function addContact(name, email, phone) {
+//     // ...твой код
+//   }
