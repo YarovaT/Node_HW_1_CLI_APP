0e3d6c100b4b9d7a5b7ae14a9b5a63bcbcf96ca6
Tetiana Yarova <andronikova11@gmail.com>
2021-09-26 12:19:06 +0300

    Add function remove contact

--- contacts.js
+++ contacts.js
@@ -1,15 +1,8 @@
+// --unhandled-rejections=strict
 const fs = require("fs/promises");
 const path = require("path");
 const crypto = require("crypto");
-
-const isAccessible = async (path) => {
-  try {
-    await fs.access(path);
-    return true;
-  } catch (err) {
-    return false;
-  }
-};
+const chalk = require("chalk");
 
 const contactsPath = path.join(__dirname, "db", "contacts.json");
 
@@ -23,25 +16,44 @@ const readContacts = async () => {
   }
 };
 
-// TODO: задокументировать каждую функцию
-function listContacts() {
-  return readContacts();
+async function listContacts() {
+  console.table(await readContacts());
 }
 
 async function getContactById(contactId) {
   const contacts = await readContacts();
-  const [result] = contacts.filter((contact) => contact.id === contactId);
-  return result;
+  const contact = contacts.find(({ id }) => id === contactId);
+  if (contact) {
+    console.log(chalk.green("Contact is found!"));
+    console.table(contact);
+  } else {
+    console.log(chalk.red("Contact is not found!"));
+  }
 }
 
-function removeContact(contactId) {}
+async function removeContact(contactId) {
+  const contacts = await readContacts();
+  const filteredContacts = contacts.filter(({ id }) => id !== contactId);
+  await fs.writeFile(contactsPath, JSON.stringify(filteredContacts, null, 2));
+  console.table(filteredContacts);
+}
 
 async function addContact(name, email, phone) {
   const contacts = await readContacts();
-  const newContact = { id: crypto.randomUUID(), name, email, phone };
-  contacts.push(newContact);
+  contacts.push({
+    id: crypto.randomUUID(),
+    name,
+    email,
+    phone,
+  });
   await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
-  return newContact;
+  console.log(chalk.green("New Contact added"));
+  console.table(contacts);
 }
 
-module.exports = { listContacts, getContactById, removeContact, addContact };
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+};
--- index.js
+++ index.js
@@ -1,86 +1,25 @@
-// const argv = require("yargs").argv;
+const argv = require("yargs").argv;
 
-// // TODO: рефакторить
-// function invokeAction({ action, id, name, email, phone }) {
-//   switch (action) {
-//     case "list":
-//       // ...
-//       break;
+const contactsAPI = require("./contacts");
 
-//     case "get":
-//       // ... id
-//       break;
+const { listContacts, getContactById, removeContact, addContact } = contactsAPI;
 
-//     case "add":
-//       // ... name email phone
-//       break;
-
-//     case "remove":
-//       // ... id
-//       break;
-
-//     default:
-//       console.warn("\x1B[31m Unknown action type!");
-//   }
-// }
-
-// invokeAction(argv);
-
-const { Command } = require("commander");
-const chalk = require("chalk");
-
-const {
-  listContacts,
-  getContactById,
-  // removeContact,
-  addContact,
-} = require("./contacts.js");
-
-const program = new Command();
-program
-  .option("-a, --action <type>", "choose action")
-  .option("-i, --id <type>", "user id")
-  .option("-n, --name <type>", "user name")
-  .option("-e, --email <type>", "user email")
-  .option("-p, --phone <type>", "user phone");
-
-program.parse(process.argv);
-
-const argv = program.opts();
-
-// TODO: рефакторить
 function invokeAction({ action, id, name, email, phone }) {
   switch (action) {
     case "list":
-      listContacts()
-        .then((contacts) => console.table(contacts))
-        .catch(console.error);
+      listContacts();
       break;
 
     case "get":
-      getContactById(id)
-        .then((contact) => {
-          if (contact) {
-            console.log(chalk.green("Contact is found!"));
-            console.log(contact);
-          } else {
-            console.log(chalk.red("Contact is not found!"));
-          }
-        })
-        .catch(console.error);
+      getContactById(id);
       break;
 
     case "add":
-      addContact(name, email, phone)
-        .then((contact) => {
-          console.log(chalk.blue("Add new contact!"));
-          console.log(contact);
-        })
-        .catch(console.error);
+      addContact(name, email, phone);
       break;
 
     case "remove":
-      // ... id
+      removeContact(id);
       break;
 
     default:
